6783a3e050e55b022ad33c8fad8f27374384460e
Bryan Hite <bhite2@mail.usf.edu>
2023-01-16 20:58:31 -0500

    Created Products App + Created Product Class

---
+++ products_project/products/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    yearinventory_quantity = models.IntegerField()
+    
+    
---
+++ products_project/products_project/local_setting.py
@@ -0,0 +1,15 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure---5flf@yoftg2mv$e+^3$ag9#*ppb4@)(s(d9z4&meszm7bqyp'
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'products_database',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '#Ma23As17'
+    }
+}
