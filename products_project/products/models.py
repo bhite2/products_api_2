5bed4b5a159e53e6f2824a10f4e22ec7d2c3a7b1
Bryan Hite <bhite2@mail.usf.edu>
2023-01-16 23:34:55 -0500

    Renamed Inventory Column + Changed URLS to get POST to work

--- products_project/products/models.py
+++ products_project/products/models.py
@@ -6,6 +6,6 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    yearinventory_quantity = models.IntegerField()
+    inventory_quantity = models.IntegerField()
     
     
