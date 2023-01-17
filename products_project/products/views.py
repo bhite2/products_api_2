83f4f63e2880b0d70c0790e1496d1ad1322f609b
Bryan Hite <bhite2@mail.usf.edu>
2023-01-16 22:00:12 -0500

    Created product Searlizer

--- products_project/products/views.py
+++ products_project/products/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import CarsSerializer
+from .models import Cars
